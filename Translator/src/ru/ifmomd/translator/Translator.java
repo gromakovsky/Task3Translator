2cc389161a2a0171e876c12c5df2f8fd1ae3c430
Dmitry <wotopul@gmail.com>
2012-10-15 19:13:31 +0400

    add Translator.java

---
+++ Translator/src/ru/ifmomd/translator/Translator.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.net.*;
+
+public class Translator {
+	private final static String apiURL = "http://translate.yandex.ru/tr.json/translate?lang=en-ru&text=";
+
+	public static String getTranslation(String source) throws IOException {
+
+		URL yandexTranslate = new URL(apiURL + source);
+		URLConnection uc = yandexTranslate.openConnection();
+		uc.connect();
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(
+				uc.getInputStream(), "UTF-8"));
+		String translation;
+		translation = in.readLine();
+		in.close();
+
+		return translation;
+	}
+
+	public static void main(String[] args) throws Exception {
+		String tr = getTranslation("hello");
+		System.out.println(tr);
+	}
+}
